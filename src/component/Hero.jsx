8d96eaab93687c61bfb08248d934a9ef6fff8b9c
Gazi Akter <gaziakterdev@gmail.com>
2024-11-16 09:59:40 +0600

    react function

--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -1,11 +1,17 @@
 
 const Hero = () => {
 
-    let mark = 90;
+    let mark = 83;
     return (
         <div>
             {mark>=80?<h1>Brilliant Result</h1>:<h1>You are faild</h1>}
             <button onClick={()=>alert("Hello")}>Submit</button>
+
+            {(()=>{
+                if(mark>80 && mark<90){
+                    return "Your result is A+"
+                }
+            })()}
         </div>
     );
 };
