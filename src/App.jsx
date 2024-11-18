bece1934e9b04b81775c3f3bd411d2270d473970
Gazi Akter <gaziakterdev@gmail.com>
2024-11-18 08:12:49 +0600

    prevenDefault method

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ const App = () => {
   return (
     <div>
       <Header/>
-      <Hero click={handleClick}/>
+      <Hero/>
       <Footer/>
     </div>
   );
--- src/component/Hero.jsx
+++ src/component/Hero.jsx
@@ -1,10 +1,17 @@
 
 const Hero = (props) => {
 
+    const postForm=(event) =>{
+        event.preventDefault();
+        alert('Form submited')
+    }
  
     return (
         <div>
-            <button onClick={props.click} >Submit</button>
+           <form onSubmit={postForm}>
+                <input type="text" placeholder="Name" />
+                <button type="submit">Submit</button>
+           </form>
         </div>
     );
 };
